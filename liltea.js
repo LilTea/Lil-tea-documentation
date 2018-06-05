b4c02f8cceddf45e8e5e0b35019db5380ee94903
Vladislav <vladdislav.georgiev@gmail.com>
2018-06-05 13:31:17 +0300

    fixed the buttons

--- liltea.js
+++ liltea.js
@@ -7,6 +7,18 @@ import {completeLiterals} from "./lib/preLexer.js"
 import {lil2jsToken} from "./lib/converters.js  "
 window.onload = ()=>{
     $('#exec').on('click', mainFunc)
+    $('.nav').children().click(changeActive); 
+    $('input[type=image]').click(showExpl);
+    $($('#grid').children()[0]).children().filter((i,tr) => tr.className== "" ).each(
+        function(i, tr){  
+            if(i %  2 == 1){
+                $(tr).attr("bgcolor", "#F46524")
+            }
+        }
+    )
+    $(".button").on("click", function(e){
+        $(`#` + $(this).attr("value")).fadeIn().delay(3000).fadeOut()  
+    })
 }
 function appendToRes(output){
     
@@ -25,4 +37,33 @@ function mainFunc() {
         completeLiterals(source, tokens), eng,
         lil2jsToken,
         tokens);
+}
+
+function showResult(e){
+    e.preventDefault();
+}
+function changeActive(){
+    let toScrollto =  $(this).html().replace(/\s/g, '');
+    let toScrollElement = $(`.${toScrollto}`);
+    
+    let offset = $(toScrollElement).offset().top
+    window.scroll({
+        top: offset - 10, 
+        behavior: 'smooth' 
+    });
+    
+    
+    $('.active').removeAttr('class');
+    $(this).addClass('active');
+}
+function showExpl(){
+    console.log('asd')
+    let exlp = $(this).parent().parent().next()
+    if($(this).parent().parent().next().is(':visible')){
+        exlp.fadeOut()
+        $(this).attr("src","index.png")
+    }else{
+        exlp.fadeIn()
+        $(this).attr("src","minus.png")
+    }
 }
